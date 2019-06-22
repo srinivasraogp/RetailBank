bfdf5395474399c29b348679f5335685e30e7394
marshadm <arshadmohd666@gmail.com>
2019-06-22 18:46:53 +0530

    commit by sri

--- src/main/java/com/hcl/service/ManageServiceImpl.java
+++ src/main/java/com/hcl/service/ManageServiceImpl.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.hcl.dto.RegistrationDetailsResDTO;
 import com.hcl.entity.Registration;
+import com.hcl.exception.DataNotFoundException;
 import com.hcl.repository.ManageRepository;
 import com.hcl.util.RandomNumberGen;
 @Service
@@ -16,6 +19,11 @@ public class ManageServiceImpl implements ManageService{
 
 	@Autowired
 	ManageRepository adminRepository;
+	
+
+	@Autowired
+	public JavaMailSender emailSender;
+
 	@Override
 	public List<RegistrationDetailsResDTO> getRegistrationDetails() {
 		List<RegistrationDetailsResDTO> registrationDetailsResDTO = new ArrayList<>();
@@ -34,9 +42,25 @@ public class ManageServiceImpl implements ManageService{
 		   reg.setUserId(RandomNumberGen.getUserIdNumber());
 		   reg.setAccountNumber(RandomNumberGen.getAccountNumber());
 		   reg.setPassword(RandomNumberGen.getPassWord(reg.getFirstName()));
+		}else {
+			throw new DataNotFoundException("User not found to approve");
 		}
-		adminRepository.save(reg);		
+		adminRepository.save(reg);	
+		try{
+			sendMail(reg.getUserId(),reg.getPassword(),reg.getFirstName());
+			System.out.println("Email sent successfully...");
+		}catch(Exception e){
+			System.out.println(e);
+		}
 		return "User Approved Successfully";
+	}
+	
+	public void sendMail(Long userId,String password,String name) throws Exception{
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo("srao79@gmail.com");
+		message.setSubject("");
+		message.setText("Hi " + name + " , please use these credentionals : userID : " +userId+" password : "+password);
+		emailSender.send(message);
 	}
 
 }
