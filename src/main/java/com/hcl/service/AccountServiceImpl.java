e095c829e7b599f5d2d4f6e3c6512c993f3f7d58
marshadm <arshadmohd666@gmail.com>
2019-06-23 20:13:09 +0530

    by sri

--- src/main/java/com/hcl/service/AccountServiceImpl.java
+++ src/main/java/com/hcl/service/AccountServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
