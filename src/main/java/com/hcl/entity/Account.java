b49f59c121ed9135d50eb6743e1eba49e6a0e045
Lakshmanpoy <user1@LP-BL8G8R1.HCLT.CORP.HCL.IN>
2019-06-23 13:08:07 +0530

    commit by inba

---
+++ src/main/java/com/hcl/entity/Account.java
@@ -0,0 +1,69 @@
+package com.hcl.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+
+	private long id;
+
+	private String userId;
+	private String accountNumber;
+	private String accountType;
+	private long balance;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getAccountType() {
+		return accountType;
+	}
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	public long getBalance() {
+		return balance;
+	}
+	public void setBalance(long balance) {
+		this.balance = balance;
+	}
+	public Account(long id, String userId, String accountNumber, String accountType, long balance) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.accountNumber = accountNumber;
+		this.accountType = accountType;
+		this.balance = balance;
+	}
+	public Account() {
+		super();
+	}
+	
+
+}
