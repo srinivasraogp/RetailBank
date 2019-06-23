2c1443adf098a2b0e060138bd990266c91daa4de
Lakshmanpoy <user1@LP-BL8G8R1.HCLT.CORP.HCL.IN>
2019-06-23 17:54:47 +0530

    commited

--- src/main/java/com/hcl/service/AccountServiceImpl.java
+++ src/main/java/com/hcl/service/AccountServiceImpl.java
@@ -1,23 +1,48 @@
 package com.hcl.service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hcl.dto.AccountRequestDTO;
+import com.hcl.dto.AccountResponseDTO;
 import com.hcl.entity.Account;
 import com.hcl.repository.AccountRepository;
 
 @Service
+@Transactional
 public class AccountServiceImpl implements AccountService  {
 @Autowired
 AccountRepository accountRepository;
 	@Override
-	public Account createAccount(AccountRequestDTO accountRequestDTO) {
-		Account account=new Account();
-		BeanUtils.copyProperties(accountRequestDTO, account);
-		return accountRepository.save(account) ;
-
+	public AccountResponseDTO createAccount(AccountRequestDTO accountRequestDTO) {
+	List<Account> account=	accountRepository.findByUserId(accountRequestDTO.getUserId());
+	for (Account account2 : account) {
+		
+		if(account2.getAccountType().equals("savings")) {
+			AccountResponseDTO accountResponseDTO=new AccountResponseDTO("savings account is already exist");
+			return accountResponseDTO;
+			
+		}if(account2.getAccountType().equals("current")) {
+			AccountResponseDTO accountResponseDTO=new AccountResponseDTO("current account is already exist");
+			return accountResponseDTO;
+			
+		}
+		else
+		{
+			account2.setAccountType(accountRequestDTO.getAccountType());
+			accountRepository.save(account2);
+			AccountResponseDTO accountResponseDTO=new AccountResponseDTO(" account created succefully");
+			return accountResponseDTO;
+			
+		}
+	}
+	AccountResponseDTO accountResponseDTO=new AccountResponseDTO();
+	return accountResponseDTO;
 		
 	}
 
