1de5155a1ea02af34a3408d66113d8d0d0d83299
marshadm <arshadmohd666@gmail.com>
2019-06-23 12:54:26 +0530

    commit by sri

---
+++ src/main/java/com/hcl/entity/Transaction.java
@@ -0,0 +1,75 @@
+package com.hcl.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Transaction {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO) 
+	private Long tansactionId;
+	@Column
+	private Long fromAccount;
+	@Column
+	private Long toAccount;
+	@Column
+	private String description;
+	public Long getTansactionId() {
+		return tansactionId;
+	}
+
+	public void setTansactionId(Long tansactionId) {
+		this.tansactionId = tansactionId;
+	}
+
+	public Long getFromAccount() {
+		return fromAccount;
+	}
+
+	public void setFromAccount(Long fromAccount) {
+		this.fromAccount = fromAccount;
+	}
+
+	public Long getToAccount() {
+		return toAccount;
+	}
+
+	public void setToAccount(Long toAccount) {
+		this.toAccount = toAccount;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getTransactionDate() {
+		return transactionDate;
+	}
+
+	public void setTransactionDate(Date transactionDate) {
+		this.transactionDate = transactionDate;
+	}
+
+	public BigDecimal getTransferAmount() {
+		return transferAmount;
+	}
+
+	public void setTransferAmount(BigDecimal transferAmount) {
+		this.transferAmount = transferAmount;
+	}
+
+	@Column
+	private Date transactionDate;
+	
+	@Column
+	private BigDecimal transferAmount;
+
+}
